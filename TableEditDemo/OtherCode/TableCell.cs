6d160c9c4663e42b310e5f25df1cd95904a6c27c
Steve <Steve@SFR-2018-XPS-15>
2023-06-11 17:10:15 -0400

    Add project files.

---
+++ TableEditDemo/OtherCode/TableCell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TableEditDemo
+    {
+    public class TableCell
+    {
+        public string ColName { get; set; } = string.Empty;
+        public string CSSClass { get; set; } = string.Empty;
+        public int Index_Col { get; set; }
+        public int Index_Row { get; set; }
+        public decimal Value_Dec { get; set; }
+        public string Value_Formatted { get; set; } = string.Empty;
+
+        public TableCell() 
+            {
+            }
+        }
+}
